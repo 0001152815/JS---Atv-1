7f751519476084e466e676868e2ab64f439a8e16
Lucas <0001152815@gmail.com.br>
2026-03-09 11:00:02 -0300

    exercício 2

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1.Botão de Boas-vindas
+const botao = document.getElementById("botao");
+	
+	botao.addEventListener("click", function() {
+		alert("Bem-vindo ao meu site desenvolvido com HTML, CSS e Javascript!")
+	});
+
+// 2.Botão para alterar texto
+const botao1 = document.getElementById("botao1");
+const texto = document.getElementById("texto");
+
+	botao1.addEventListener("click", function() {
+		texto.innerText = "Este site possui interatividade com Javascript!";
+	});
+// 3.Botão para alterar a cor de um elemento
+
+// 4.
+// 5.
+// 6.
